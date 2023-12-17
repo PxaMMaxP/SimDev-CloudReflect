1ee3303fb57090b036255176de19e8e4c86e2c81
Max P <46793832+PxaMMaxP@users.noreply.github.com>
2023-12-17 14:35:45 +0100

    V 0.1 (#1)
    * Erste Version
    
    * Berrechtigungen korrigiert.
    
    * Bugfixing
    
    * Überwachung auf alle Ordner ausgeweitet.
    
    * install.sh Skript so angepasst, dass ohne Argumente die Hilfe angezeigt wird.
    
    * Service auf root umgestellt.
    
    * Version 0.1

---
+++ cloud-www-sync.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/config"
+
+echo "### Start script 'cloud-www-sync' Version 0.1 ###"
+
+# Funktion zum Synchronisieren von Inhalten
+sync_content() {
+    local source_path="$1"
+    local target_path="$2"
+
+    # Unison Synchronisation
+    unison -auto -batch -prefer newer "$source_path" "$target_path"
+}
+
+# Funktion zum Aktualisieren von Berechtigungen
+update_permissions() {
+    local path="$1"
+
+    # Berechtigungen und Besitzer ändern
+    find "$path" -exec chmod 0766 {} \;
+    chown -R 33:33 "$path"
+}
+
+# Funktion zur Überwachung und Synchronisation
+monitor_and_sync() {
+    while true; do
+        # Warten auf Dateiänderungen
+        inotifywait -r -e modify,create,delete,move \
+            "${NEXTCLOUD_DATA_PATH}${NEXTCLOUD_PATH}" \
+            "${CONTAINER_PATH}/content" \
+            "${CONTAINER_PATH}/assets"
+
+        # Synchronisation und Aktualisierung
+        sync_content "${NEXTCLOUD_DATA_PATH}${NEXTCLOUD_PATH}/content" "${CONTAINER_PATH}/content"
+        sync_content "${NEXTCLOUD_DATA_PATH}${NEXTCLOUD_PATH}/assets" "${CONTAINER_PATH}/assets"
+
+        # Nextcloud-Dateien scannen
+        docker exec --user www-data nextcloud-app php occ files:scan --path="${NEXTCLOUD_PATH}/"
+
+        # Berechtigungen und Besitzer aktualisieren
+        update_permissions "${CONTAINER_PATH}/content"
+        update_permissions "${CONTAINER_PATH}/assets"
+    done
+}
+
+# Hauptteil des Skripts
+monitor_and_sync
+
+echo "### End script 'cloud-www-sync' ###"
