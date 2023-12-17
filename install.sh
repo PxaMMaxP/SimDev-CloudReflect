e061cdbce1e4e06d550cd743f125740ac9658a04
Max P <Mail@MPassarello.de>
2023-12-17 19:30:42 +0100

    install.sh vor weiterer Refaktorirung.

--- install.sh
+++ install.sh
@@ -1,108 +1,236 @@
 #!/bin/bash
 
-# Standardpfade
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-CONFIG_FILE="${SCRIPT_DIR}/config"
-BIN_DIR="/usr/local/bin"
+# Define script directory and import configuration, logging, and version information
+SCRIPT_DIR="$( cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/logging"
+source "${SCRIPT_DIR}/config"
+source "${SCRIPT_DIR}/version"
 
-# Hilfe-Funktion
+# Initialize global variables
+SERVICE_NAME=""
+BIN_DIR=""
+
+# Function to display help information
 show_help() {
+    echo "${APP_NAME} Version ${VERSION}"
     echo "Usage: $0 [options]"
     echo
     echo "Options:"
-    echo "  -h, --help                   Show this help message and exit."
-    echo "  --install                    Install the script and create systemd service."
-    echo "  --uninstall                  Uninstall the script and systemd service."
+    echo "  -h, --help     Show this help message and exit."
+    echo "  -l, --log      Set log level (default: info). Possible values: info, error, trace"
+    echo "  --install      Install the script and create a systemd service."
+    echo "  --uninstall    Uninstall the script and systemd service."
     exit 0
 }
 
-# Service-Name aus der Konfigurationsdatei lesen
+# Function to read the service name from the configuration file
 read_service_name() {
     source "${CONFIG_FILE}"
     echo "${SERVICE_NAME}"
 }
 
-# Install-Funktion
-install() {
-    echo "Starting installation..."
+# Function to update the SERVICE_NAME in the configuration file
+update_service_name_in_config() {
+    # Update or add SERVICE_NAME in the config file
+    if grep -q "SERVICE_NAME=" "${CONFIG_FILE}"; then
+        # Updating existing SERVICE_NAME
+        if sed_output=$(sed -i "s/SERVICE_NAME=\".*\"/SERVICE_NAME=\"$SERVICE_NAME\"/" "${CONFIG_FILE}" 2>&1); then
+            log_message "trace" "SERVICE_NAME updated in config: $CONFIG_FILE"
+        else
+            error=$sed_output
+            log_message "error" "Error updating SERVICE_NAME in config: $error"
+            return 1
+        fi
+    else
+        # Adding new SERVICE_NAME
+        if echo_output=$(echo "SERVICE_NAME=\"$SERVICE_NAME\"" >> "${CONFIG_FILE}" 2>&1); then
+            log_message "trace" "SERVICE_NAME added to config: $CONFIG_FILE"
+        else
+            error=$echo_output
+            log_message "error" "Error adding SERVICE_NAME to config: $error"
+            return 1
+        fi
+    fi
+}
 
-    # Service-Name abfragen
-    read -p "Enter the name for the systemd service (e.g., cloud-sync): " SERVICE_NAME
+# Function to request and set the binary directory
+bin_dir_demand() {
+    # Detect and set the binary directory
+    local detected_bin_dir=$(echo $PATH | tr ':' '\n' | grep -m 1 '/usr/local/bin')
+    local fallback_bin_dir="/usr/local/bin"
+    local bin_dir=${detected_bin_dir:-$fallback_bin_dir}
 
-    # Update SERVICE_NAME in der Konfigurationsdatei
-	if grep -q "SERVICE_NAME=" "${CONFIG_FILE}"; then
-		# SERVICE_NAME in der Datei aktualisieren
-		sed -i "s/SERVICE_NAME=\".*\"/SERVICE_NAME=\"$SERVICE_NAME\"/" "${CONFIG_FILE}"
-	else
-		# SERVICE_NAME zur Datei hinzufügen
-		echo "SERVICE_NAME=\"$SERVICE_NAME\"" >> "${CONFIG_FILE}"
-	fi
+    # Prompt the user to confirm or enter a new path
+    read -p "Enter the installation directory for the script [$bin_dir]: " input_dir
+    if [ -w "${input_dir:-$bin_dir}" ]; then 
+        log_message "trace" "Binary path writable: ${input_dir:-$bin_dir}"
+    else 
+        log_message "error" "Binary path not writable: ${input_dir:-$bin_dir}"
+        exit 1
+    fi
 
-    # Erstellen der systemd Service-Datei
+    BIN_DIR=${input_dir:-$bin_dir}
+}
+
+# Function to request and set the service name
+service_name_demand() {
+    # Request and validate the service name
+    read -p "Enter the name for the systemd service [$APP_NAME]: " service_name
+    if systemctl list-unit-files "$service_name" &>/dev/null; then
+        log_message "error" "Service exists: ${service_name}"
+        exit 1
+    else
+        log_message "trace" "Service name available: ${service_name}"
+        SERVICE_NAME=${service_name}
+    fi
+}
+
+# Function to create a systemd service file
+create_systemd_service_file() {
     SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
-    cat > "${SERVICE_FILE}" <<EOF
+    if cat_output=$(cat > "${SERVICE_FILE}" <<EOF
 [Unit]
-Description=Cloud WWW Sync Service
+Description=${APP_NAME} Sync Service
 After=docker.service network.target
 PartOf=docker.service
 
 [Service]
-ExecStart=${BIN_DIR}/cloud-www-sync.sh
+ExecStart=${BIN_DIR}/cloud-reflect.sh
 Restart=on-failure
 User=root
 
 [Install]
 WantedBy=multi-user.target
 EOF
+    ); then
+        log_message "trace" "Systemd service file created: $SERVICE_FILE"
+    else
+        error=$?
+        log_message "error" "Error creating systemd service file: $SERVICE_FILE. Status: $error"
+        exit 1
+    fi
 
-    # Setzen der Berechtigungen für die Service-Datei
-    chmod 644 "${SERVICE_FILE}"
+    # Set permissions for the service file
+    if chmod_output=$(chmod 644 "${SERVICE_FILE}" 2>&1); then
+        log_message "trace" "Service file permissions set: $SERVICE_FILE"
+    else
+        error=$chmod_output
+        log_message "error" "Error setting permissions: $SERVICE_FILE. Error: $error"
+    fi
 
-    # Systemd daemon neu laden
-    systemctl daemon-reload
+    # Reload systemd daemon
+    if systemd_output=$(systemctl daemon-reload 2>&1); then
+        log_message "trace" "Systemd daemon reloaded"
+    else
+        error=$systemd_output
+        log_message "error" "Error reloading systemd daemon: $error"
+    fi
+
+    # Enable the service
+    if systemctl_output=$(systemctl enable "${SERVICE_NAME}" 2>&1); then
+        log_message "trace" "Service enabled: ${SERVICE_NAME}"
+    else
+        error=$systemctl_output
+        log_message "error" "Error enabling service: ${SERVICE_NAME}. Error: $error"
+    fi
+}
+
+# Install function
+install() {
+    # Confirm installation
+    read -p "Confirm installation of ${APP_NAME} ${VERSION}? [y/N]: " confirmation
+    if [[ ! "$confirmation" =~ ^[Yy]$ ]]; then
+        echo "Installation canceled."
+        exit 0
+    fi
+
+    echo "Installing ${APP_NAME} ${VERSION}..."
+    bin_dir_demand
 	
-	# Erstellen des symbolischen Links für das Skript
-    ln -sf "${SCRIPT_DIR}/cloud-www-sync.sh" "${BIN_DIR}/cloud-www-sync.sh"
+	if lnsf_output=$(ln -sf "${SCRIPT_DIR}/cloud-reflect.sh" "${BIN_DIR}/cloud-reflect.sh" 2>&1); then
+        log_message "trace" "Create the symbolic link for the script successfully: Source:${SCRIPT_DIR}/cloud-reflect.sh; Target:${BIN_DIR}/cloud-reflect.sh."
+    else
+        error=$lnsf_output
+        log_message "error" "Creating the symbolic link for the script was not successful: Source:${SCRIPT_DIR}/cloud-reflect.sh; Target:${BIN_DIR}/cloud-reflect.sh. Error: $error"
+		exit 1
+    fi
+	
+    service_name_demand
+    create_systemd_service_file
 
-    # Den Service nicht aktivieren
-    systemctl enable "${SERVICE_NAME}"
+    if ! update_service_name_in_config; then
+        echo "Error updating service name in config."
+        exit 1
+    fi
 
-    echo "Installation completed. Service created and enabled."
+    echo "${APP_NAME} ${VERSION} installed."
 }
 
-# Uninstall-Funktion
+# Uninstall function
 uninstall() {
-    echo "Uninstalling script and systemd service..."
+    # Confirm uninstallation
+    read -p "Confirm uninstallation of ${APP_NAME}? [y/N]: " confirmation
+    if [[ ! "$confirmation" =~ ^[Yy]$ ]]; then
+        echo "Uninstallation canceled."
+        exit 0
+    fi
 
-     # Service-Name aus der Konfigurationsdatei lesen
+    # Read service name from config
     SERVICE_NAME=$(read_service_name)
+    SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
 
-    # Den Service stoppen und deaktivieren
-    systemctl stop "${SERVICE_NAME}"
-    systemctl disable "${SERVICE_NAME}"
+    # Stop and disable the service
+    if systemctl_output=$(systemctl stop "${SERVICE_NAME}" 2>&1); then
+        log_message "trace" "Service stopped: ${SERVICE_NAME}"
+    else
+        error=$systemctl_output
+        log_message "error" "Error stopping service: ${SERVICE_NAME}. Error: $error"
+    fi
 
-    # Entfernen des symbolischen Links
-    rm -f "${BIN_DIR}/cloud-www-sync.sh"
+    if systemctl_output=$(systemctl disable "${SERVICE_NAME}" 2>&1); then
+        log_message "trace" "Service disabled: ${SERVICE_NAME}"
+    else
+        error=$systemctl_output
+        log_message "error" "Error disabling service: ${SERVICE_NAME}. Error: $error"
+    fi
 
-    # Service-Datei entfernen
-    rm -f "${SERVICE_FILE}"
+    # Remove symbolic link and service file
+    if rm_output=$(rm -f "${BIN_DIR}/cloud-reflect.sh" 2>&1); then
+        log_message "trace" "Symbolic link removed: ${BIN_DIR}/cloud-reflect.sh"
+    else
+        error=$rm_output
+        log_message "error" "Error removing symbolic link: ${BIN_DIR}/cloud-reflect.sh. Error: $error"
+    fi
 
-    # Systemd daemon neu laden
-    systemctl daemon-reload
+    if rm_output=$(rm -f "${SERVICE_FILE}" 2>&1); then
+        log_message "trace" "Service file removed: ${SERVICE_FILE}"
+    else
+        error=$rm_output
+        log_message "error" "Error removing service file: ${SERVICE_FILE}. Error: $error"
+    fi
 
-    echo "Uninstallation completed."
+    # Reload systemd daemon
+    if systemd_output=$(systemctl daemon-reload 2>&1); then
+        log_message "trace" "Systemd daemon reloaded"
+    else
+        error=$systemd_output
+        log_message "error" "Error reloading systemd daemon: $error"
+    fi
+
+    echo "${APP_NAME} uninstalled."
 }
 
-# Überprüfen, ob Argumente übergeben wurden
+# Check for passed arguments
 if [ "$#" -eq 0 ]; then
     show_help
     exit 0
 fi
 
-# Argumente verarbeiten
+# Process arguments
 while [ "$#" -gt 0 ]; do
     case "$1" in
         -h|--help) show_help ;;
+        -l|--log) LOG_LEVEL="$2"; shift ;;
         --install) install; exit 0 ;;
         --uninstall) uninstall; exit 0 ;;
         *) echo "Unknown option: $1"; show_help ;;
